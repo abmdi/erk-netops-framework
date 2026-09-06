bad5a07eb5fc72ec71e48f0048b3e19b77effeb0
abmdi <absmrd@gmail.com>
2026-09-06 17:07:21 +0000

    Add Terraform module for creating an AWS S3 bucket with versioning and logging

--- terraform/modules/s3_bucket/main.tf
+++ terraform/modules/s3_bucket/main.tf
@@ -1,10 +1,9 @@
+hcl
 provider "aws" {
-  # Specify the AWS region
   region = var.region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  # Name of the S3 bucket
+resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 
   # Enable versioning for the bucket
@@ -12,44 +11,73 @@ resource "aws_s3_bucket" "my_bucket" {
     enabled = true
   }
 
-  # Define server-side encryption configuration
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+  # Enable server access logging
+  logging {
+    target_bucket = var.logging_target_bucket
+    target_prefix = "${var.bucket_name}/logs/"
+  }
+
+  # Configure bucket policy to allow logging
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "logging.s3.amazonaws.com"
+        },
+        Action = "s3:PutObject",
+        Resource = "arn:aws:s3:::${var.logging_target_bucket}/${var.bucket_name}/logs/*",
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = var.account_id
+          },
+          ArnLike = {
+            "aws:SourceArn" = "arn:aws:s3:::${aws_s3_bucket.this.bucket}"
+          }
+        }
       }
-    }
-  }
+    ]
+  })
 
-  # Block public access to the bucket
-  block_public_access {
-    block_public_acls       = true
-    ignore_public_acls      = true
-    block_public_policy     = true
-    restrict_public_buckets = true
-  }
-
-  tags = {
-    # Tagging for resource identification and cost management
-    Name        = var.bucket_name
-    Environment = var.environment
-  }
+  # Enforce SSL for all communications with the bucket
+  bucket_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Deny",
+        Principal = "*",
+        Action = "s3:*",
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.this.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.this.bucket}/*"
+        ],
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = false
+          }
+        }
+      }
+    ]
+  })
 }
 
 variable "region" {
-  # Define the AWS region for the S3 bucket
-  description = "The AWS region to deploy resources in"
+  description = "The AWS region to deploy the bucket in."
   type        = string
 }
 
 variable "bucket_name" {
-  # Define the S3 bucket name
-  description = "The name of the S3 bucket"
+  description = "The name of the S3 bucket to create."
   type        = string
 }
 
-variable "environment" {
-  # Define the deployment environment (e.g., dev, prod)
-  description = "The environment for resource deployment"
+variable "logging_target_bucket" {
+  description = "The name of the S3 bucket where access logs should be stored."
+  type        = string
+}
+
+variable "account_id" {
+  description = "The AWS account ID for policy conditions."
   type        = string
 }
