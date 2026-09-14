2db140e86e8a8090ae6df82e58cebd255e80da0b
abmdi <absmrd@gmail.com>
2026-09-14 19:27:01 +0000

    Add Terraform module for AWS Transit Gateway setup

--- terraform/modules/transit_gateway/main.tf
+++ terraform/modules/transit_gateway/main.tf
@@ -1,26 +1,34 @@
 hcl
-// Define a Terraform module to create an AWS Transit Gateway
+// Define a module for creating an AWS Transit Gateway
+variable "tgw_name" {
+  description = "Name of the Transit Gateway"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region where the Transit Gateway should be created"
+  type        = string
+}
 
 provider "aws" {
-  region = "us-east-1" // Specify the AWS region
+  region = var.region
 }
 
 resource "aws_ec2_transit_gateway" "this" {
-  description = "Main Transit Gateway for centralized network management" // Description for the Transit Gateway
-  
-  // Set the default route table association and propagation
-  default_route_table_association = "enable"
-  default_route_table_propagation = "enable"
+  description = var.tgw_name
 
-  // Specify tags for identifying and managing the resource
-  tags = {
-    Name = "Main-Transit-Gateway"
-    Environment = "Production"
-  }
+  // Enable default route table association and propagation
+  default_route_table_association = true
+  default_route_table_propagation = true
+
+  // Enable DNS support and VPN ECMP support
+  dns_support        = "enable"
+  vpn_ecmp_support   = "enable"
+  auto_accept_shared_attachments = "disable" // Disable auto-accept of shared attachments for security
 }
 
 // Output the ID of the created Transit Gateway
 output "transit_gateway_id" {
-  description = "ID of the created Transit Gateway" // Describe the output variable
+  description = "ID of the created Transit Gateway"
   value       = aws_ec2_transit_gateway.this.id
 }
